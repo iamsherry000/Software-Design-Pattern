35b684816c787c853b397415445e5aa7375c4bbc
iamsherry000 <39553867+iamsherry000@users.noreply.github.com>
2024-03-21 08:13:30 +0000

    create cards and deck

--- ShowDown_Java/Deck.java
+++ ShowDown_Java/Deck.java
@@ -1,5 +1,14 @@
 package ShowDown_Java;
 
+import java.util.Stack;
+import java.util.Collections;
+
 public class Deck {
-    
+    private Stack<Card> stackCard = new Stack();
+
+    public Deck initialCards(){
+        Deck deck = new Deck();
+        
+        return deck;
+    }
 }
--- ShowDown_Java/Main.java
+++ ShowDown_Java/Main.java
@@ -1,5 +1,7 @@
 package ShowDown_Java;
 
+import static ShowDown_Java.Deck.initialCards;
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
